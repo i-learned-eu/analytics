043e5d10a6efb823357058f43561ae59fdb0eb94
Eban Rami <account+github@eban.bzh>
2022-01-30 16:06:34 +0100

    [🚀 Feature] Shipped two new features ! Month and Year view

--- static/js/script.js
+++ static/js/script.js
@@ -96,8 +96,12 @@ fetch("data.json")
           viewByDay.push(json.visitors.data[i].visitors.count)
           i--;
         }
-
         ctx = document.getElementById('graphCanvas').getContext('2d');
+        document.querySelector(".button.active").classList.remove("active")
+        document.querySelectorAll(".button")[0].classList.add("active")
+        if (typeof myChart != "undefined") {
+          myChart.destroy();
+        }
         myChart = new Chart(ctx, {
           type: 'bar',
           data: {
@@ -112,7 +116,7 @@ fetch("data.json")
               borderRadius: 15,
               borderSkipped: false,
               barThickness: 12
-            }]
+          }]
           },
           options: {
             plugins: {
@@ -139,4 +143,159 @@ fetch("data.json")
       });
   }
 
+  function updateGraphMonths() {
+    fetch("data.json")
+      .then(response => response.json())
+      .then(function(json) {
+        document.querySelector(".button.active").classList.remove("active")
+        document.querySelectorAll(".button")[1].classList.add("active")
+
+        months = []
+        viewByMonth = []
+        i = 0
+        currentMonth = json.visitors.data[i].data.slice(4, -2);
+        ii = 0;
+        while (i < 12) {
+          thisMonthView = 0
+          while (json.visitors.data[ii].data.slice(4, -2) == currentMonth) {
+            if (ii < json.visitors.data.length - 1) {
+              thisMonthView += json.visitors.data[ii].visitors.count
+              ii++;
+            } else {
+              break;
+            }
+          }
+
+          currentMonth = json.visitors.data[ii - 1].data.slice(4, -2)
+
+          viewByMonth.push(thisMonthView)
+          months.push(currentMonth)
+
+          currentMonth = json.visitors.data[ii].data.slice(4, -2)
+
+          i++;
+        }
+        viewByMonth.reverse();
+        months.reverse();
+
+        ctx = document.getElementById('graphCanvas').getContext('2d');
+        myChart.destroy();
+        myChart = new Chart(ctx, {
+          type: 'bar',
+          data: {
+            labels: months,
+            datasets: [{
+              label: 'Visites ce mois',
+              data: viewByMonth,
+              backgroundColor: [
+                '#D83D3D',
+                "#973333"
+              ],
+              borderRadius: 15,
+              borderSkipped: false,
+              barThickness: 12
+          }]
+          },
+          options: {
+            plugins: {
+              legend: {
+                display: false
+              },
+            },
+            scales: {
+              y: {
+                beginAtZero: true,
+                grid: {
+                  display: false
+                },
+              },
+              x: {
+                beginAtZero: true,
+                grid: {
+                  display: false
+                },
+              },
+            },
+          }
+        });
+      });
+  }
+
+  function updateGraphYears() {
+    fetch("data.json")
+      .then(response => response.json())
+      .then(function(json) {
+        document.querySelector(".button.active").classList.remove("active")
+        document.querySelectorAll(".button")[2].classList.add("active")
+
+        years = []
+        viewByYear = []
+        i = 0
+        currentYear = json.visitors.data[i].data.slice(2, -4);
+        ii = 0;
+        while (i <= json.visitors.data[0].data.slice(2, -4) - json.visitors.data.at(-1).data.slice(2, -4)) {
+          thisYearView = 0
+          while (json.visitors.data[ii].data.slice(2, -4) == currentYear) {
+            if (ii < json.visitors.data.length - 1) {
+              thisYearView += json.visitors.data[ii].visitors.count;
+              ii++;
+            } else {
+              break;
+            }
+          }
+
+          currentYear = json.visitors.data[ii - 1].data.slice(2, -4)
+
+          viewByYear.push(thisYearView)
+          years.push(currentYear)
+
+          currentYear = json.visitors.data[ii].data.slice(2, -4)
+
+          i++;
+        }
+        viewByYear.reverse();
+        years.reverse();
+
+        ctx = document.getElementById('graphCanvas').getContext('2d');
+        myChart.destroy();
+        myChart = new Chart(ctx, {
+          type: 'bar',
+          data: {
+            labels: years,
+            datasets: [{
+              label: 'Visites ce mois',
+              data: viewByYear,
+              backgroundColor: [
+                '#D83D3D',
+                "#973333"
+              ],
+              borderRadius: 15,
+              borderSkipped: false,
+              barThickness: 12
+          }]
+          },
+          options: {
+            plugins: {
+              legend: {
+                display: false
+              },
+            },
+            scales: {
+              y: {
+                beginAtZero: true,
+                grid: {
+                  display: false
+                },
+              },
+              x: {
+                beginAtZero: true,
+                grid: {
+                  display: false
+                },
+              },
+            },
+          }
+        });
+      });
+  }
 updateGraphDays()
